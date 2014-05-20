abfaf23bbd9467a185ecc67fac844f6591935a10
herculeshssj <herculeshssj@be890339-4cc5-40c0-92b7-9e524a6ac59a>
2014-05-20 13:53:46 +0000

    Elaborado diagrama de classes inicial e refeito o diagrama de entidade-relacionamento do projeto. Criado script de atualização da base. refs #1013 @1h30m
    git-svn-id: svn://hslife.com.br/hslife/imobiliariaweb/trunk@22 be890339-4cc5-40c0-92b7-9e524a6ac59a

---
+++ src/br/com/hslife/imobiliaria/db/update-script.sql
@@ -0,0 +1,74 @@
+﻿/*** 
+
+    Copyright (c) 2014 Hércules S. S. José
+    
+
+    Este arquivo é parte do programa Imobiliária Web.
+
+    Imobiliária Web é um software livre; você pode redistribui-lo e/ou 
+
+    modificá-lo dentro dos termos da Licença Pública Geral Menor GNU como 
+
+    publicada pela Fundação do Software Livre (FSF); na versão 2.1 da 
+
+    Licença.
+    
+    
+    Este programa é distribuído na esperança que possa ser util, 
+
+    mas SEM NENHUMA GARANTIA; sem uma garantia implicita de ADEQUAÇÂO a qualquer
+
+    MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral Menor GNU em 
+    
+    português para maiores detalhes.
+    
+    
+    Você deve ter recebido uma cópia da Licença Pública Geral Menor GNU sob o 
+
+    nome de "LICENSE.TXT" junto com este programa, se não, acesse o site HSlife no 
+
+    endereco www.hslife.com.br ou escreva para a Fundação do Software Livre(FSF) Inc., 
+
+    51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA.
+    
+    
+    Para mais informações sobre o programa Imobiliária Web e seus autores acesso o 
+
+    endereço www.hslife.com.br, pelo e-mail contato@hslife.com.br ou escreva para 
+
+    Hércules S. S. José, Av. Ministro Lafaeyte de Andrade, 1683 - Bl. 3 Apt 404, 
+
+    Marco II - Nova Iguaçu, RJ, Brasil.
+
+ ***/
+
+-- Script de atualização das tabelas
+
+/*** Versão 2014.1 ***/
+
+-- Tabela formapagamento
+create table formapagamento (
+id bigserial not null,
+descricao varchar(50) not null,
+primary key(id)
+);
+
+-- Tabela aluguel
+create table aluguel (
+id bigserial not null,
+periodo integer,
+ano integer,
+valor double precision,
+vencimento date,
+pagamento date,
+valorPago double precision,
+idContrato bigint,
+juros double precision,
+multa double precision,
+idFormaPagamento bigint,
+primary key(id)
+);
+
+alter table aluguel add constraint fk_formapagamento_aluguel foreign key (idFormaPagamento) references formapagamento(id);
+alter table aluguel add constraint fk_contrato_aluguel foreign key (idContrato) references contrato(id);
+ 
