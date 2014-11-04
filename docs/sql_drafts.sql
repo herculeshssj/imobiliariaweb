5aedd3d7daa2a7a64230a05b0cb63c7ac8ce1ca1
herculeshssj <herculeshssj@be890339-4cc5-40c0-92b7-9e524a6ac59a>
2014-11-04 23:29:37 +0000

    Inclusão do relatório financeiro na pasta jasper do projeto. refs #1208 @2h
    git-svn-id: svn://hslife.com.br/hslife/imobiliariaweb/trunk@73 be890339-4cc5-40c0-92b7-9e524a6ac59a

--- docs/sql_drafts.sql
+++ docs/sql_drafts.sql
@@ -79,7 +79,7 @@ id bigserial not null,
 descricao varchar(50) not null,
 primary key(id)
 );
-*/
+*
 
 select * from permissao;
 
@@ -133,3 +133,23 @@ insert into seguradora (descricao)
 update contrato c set idseguradora = (select id from seguradora where descricao = c.seguradora);
 
 alter table contrato drop column seguradora;
+*/
+
+select * from contrato;
+select * from imovel;
+select * from cliente;
+
+select distinct
+cl_lo.nome as nome_locador,
+cl_lc.nome as nome_locatario,
+i.tipoimovel,
+(select count(id) from aluguel where pagamento is null and idcontrato = a.idcontrato) as alugueis_pendentes,
+(select count(id) from aluguel where pagamento is not null and idcontrato = a.idcontrato) as alugueis_pagos
+from
+aluguel a
+inner join contrato c on c.id = a.idcontrato
+inner join cliente cl_lc on cl_lc.id = c.idlocatario
+inner join imovel i on i.id = c.idimovel
+inner join cliente cl_lo on cl_lo.id = i.idlocador
+where a.pagamento is null
+order by cl_lc.nome asc;
